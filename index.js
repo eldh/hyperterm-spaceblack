d9eb38652ec0010a0e99ecfee91035901d441f11
eldh <andreas.eldh@gmail.com>
2016-07-26 20:23:02 +0200

    0.7.0 comp

--- index.js
+++ index.js
@@ -1,84 +1,91 @@
-'use strict';
 const foregroundColor = '#eff0eb';
 const backgroundColor = '#16181c';
 const black = backgroundColor;
+const white = foregroundColor;
 const darkBlack = '#0f1114';
 const yellow = '#f5f9c1';
 const magenta = '#ff6ac1';
-const brightBlack = '#2d3646';
-const brightWhite = foregroundColor;
-
-const veryLightGray = '#c5c8c6';
-const lightGray = '#65737E';
-const gray = '#2F3640';
-const darkGray = '#282a2e';
-const veryDarkGray = '#1d1f21';
+const lightblack = '#2d3646';
+const lightWhite = foregroundColor;
+const darkGray = '#1d1f21';
 
 const cyan = '#8abeb7';
 const blue = '#81a2be';
-const purple = '#B48EAD';
 const green = '#acc697';
 const red = '#cc6666';
-const orange = '#de935f';
-const lightOrange = '#EBCB8B';
+
+const colors = {
+  black,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+  white,
+  lightblack,
+  lightRed: red,
+  lightGreen: green,
+  lightYellow: yellow,
+  lightBlue: blue,
+  lightMagenta: magenta,
+  lightCyan: cyan,
+  lightWhite,
+};
+const css = (config) => `
+  ${config.css || ''}
+  .tabs_list,
+  .tab_tab {
+    background-color: ${darkBlack};
+  }
+  .tab_active {
+    background-color: ${black};
+  }
+  .tab_textActive {
+    box-shadow: inset 0 1px 0 0px ${darkGray};
+  }
+  .tab_textActive:after,
+  .tab_textActive:before {
+    content: '';
+    height: 100%;
+    width: 1px;
+    top: 0;
+    left: 0;
+    position: absolute;
+    background-color: ${darkGray};
+  }
+  .tab_textActive:after {
+    left: calc(100% - 1px);
+  }
+  .tabs_list {
+    border-color: ${darkGray} !important;
+  }
+  .tab_active:before {
+    border-color: ${black};
+  }
+`;
 
 exports.decorateConfig = config => {
-	return Object.assign({}, config, {
-		backgroundColor,
-		foregroundColor,
-		borderColor: darkBlack,
-		cursorColor: brightWhite,
-		colors: [
-			black,
-			red,
-			green,
-			yellow,
-			blue,
-			magenta,
-			cyan,
-			gray,
+  return Object.assign({}, config, {
+    backgroundColor,
+    foregroundColor,
+    borderColor: darkBlack,
+    cursorColor: lightWhite,
+    colors,
+    css: css(config),
+  });
+};
 
-			// bright
-			brightBlack,
-			red,
-			green,
-			yellow,
-			blue,
-			magenta,
-			cyan,
-			brightWhite
-		],
-		css: `
-			${config.css || ''}
-			.tabs_list,
-			.tab_tab {
-				background-color: ${darkBlack};
-			}
-			.tab_active {
-				background-color: ${black};
-			}
-			.tab_textActive {
-				box-shadow: inset 0 1px 0 0px ${veryDarkGray};
-			}
-			.tab_textActive:after,
-			.tab_textActive:before {
-				content: '';
-				height: 100%;
-				width: 1px;
-				top: 0;
-				left: 0;
-				position: absolute;
-				background-color: ${veryDarkGray};
-			}
-			.tab_textActive:after {
-				left: calc(100% - 1px);
-			}
-			.tabs_list {
-				border-color: ${veryDarkGray} !important;
-			}
-			.tab_active:before {
-				border-color: ${black};
-			}
-		`
-	});
+// Development middleware for HMR
+exports.middleware = () => (next) => (action) => {
+  /* eslint-disable no-param-reassign, default-case */
+  switch (action.type) {
+  case 'CONFIG_LOAD':
+  case 'CONFIG_RELOAD':
+    action.config.foregroundColor = foregroundColor;
+    action.config.backgroundColor = backgroundColor;
+    action.config.cursorColor = foregroundColor;
+    action.config.colors = colors;
+  }
+  next(action);
 };
